cf1245843969f1e2aedcb08990e12ad91a54f72e
Adesh Dhande <adeshdhande9696@gmail.com>
2026-02-09 15:07:01 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
-﻿/*===== MENU SHOW =====*/
+/*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
-
+    
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
+            console.log('Menu toggled!') // Debug sathi
         })
+    } else {
+        console.error('Toggle or Nav element not found!') // Debug sathi
     }
 }
 showMenu('nav-toggle', 'nav-menu')
@@ -16,7 +19,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
+    // When we click on each nav__link, we remove the show class
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -48,10 +51,10 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-    //     reset: true
+    // reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
 sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 }); 
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
